4e86bacfebae9192ee3d4fa019cdd21801c3bd05
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-02-21 00:38:44 +0530

    Added: Retrieve Contacts from the Database that were added in a particular period

--- src/main/java/com/bridgelabz/jdbc1/Day35_Addressbook_JDBC/AddressBook.java
+++ src/main/java/com/bridgelabz/jdbc1/Day35_Addressbook_JDBC/AddressBook.java
@@ -17,8 +17,7 @@ public class AddressBook {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			System.out.println("Drivers loaded!!");
-			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?", "root",
-					"rootroot");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?", "root", "rootroot");
 			System.out.println("connection Established!!");
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
@@ -53,5 +52,79 @@ public class AddressBook {
 		}
 		return addressBookList;
 
+	}
+
+	public void updateCityByZip(String address, String city, String state, int zip, int id) {
+		try (Connection connection = getConnection()) {
+			Statement statement = connection.createStatement();
+			String query = "Update addressbook_table set address=" + "'" + address + "'" + ", " + "city=" + "'" + city
+					+ "'" + ", " + "state=" + "'" + state + "'" + ", " + "zipcode=" + zip + " where id=" + id + ";";
+			int result = statement.executeUpdate(query);
+			System.out.println(result);
+			if (result > 0) {
+				System.out.println("Address Updated Successfully");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public List<Contacts> findAllForParticularDate(LocalDate date) {
+		ResultSet resultSet = null;
+		List<Contacts> addressBookList = new ArrayList<Contacts>();
+		try (Connection connection = getConnection()) {
+			Statement statement = connection.createStatement();
+			String sql = "select * from addressbook_table where dateadded between cast(' " + date + "'"
+					+ " as date)  and date(now());";
+			resultSet = statement.executeQuery(sql);
+			int count = 0;
+			while (resultSet.next()) {
+				Contacts contactInfo = new Contacts();
+				contactInfo.setFirstName(resultSet.getString("firstName"));
+				contactInfo.setLastName(resultSet.getString("lastname"));
+				contactInfo.setAddress(resultSet.getString("address"));
+				contactInfo.setCity(resultSet.getString("city"));
+				contactInfo.setState(resultSet.getString("state"));
+				contactInfo.setZip(resultSet.getInt("zip"));
+				contactInfo.setPhoneNumber(resultSet.getString("PhoneNumber"));
+				contactInfo.setEmailId(resultSet.getString("email"));
+				contactInfo.setBookName(resultSet.getString("booknamed"));
+				contactInfo.setDateAdded(resultSet.getDate("dateadded").toLocalDate());
+
+				addressBookList.add(contactInfo);
+			}
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+		return addressBookList;
+	}
+
+	public int countByCity(String city) {
+		try (Connection connection = getConnection()) {
+			Statement statement = connection.createStatement();
+			String sql = "select count(firstname) from addressbook_table where city=" + "'" + city + "';";
+			ResultSet result = statement.executeQuery(sql);
+			result.next();
+			int count = result.getInt(1);
+
+			return count;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+
+	public int countByState(String state) {
+		try (Connection connection = getConnection()) {
+			Statement statement = connection.createStatement();
+			String sql = "select count(firstname) from addressbook_table where city=" + "'" + state + "';";
+			ResultSet result = statement.executeQuery(sql);
+			result.next();
+			int count = result.getInt(1);
+			return count;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return 0;
 	}
 }
